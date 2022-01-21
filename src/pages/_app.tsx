00b1d5726003d0f8ecba6afb5ea87be7b9a101d0
Alan Soares <alanrsoars@gmail.com>
2022-01-22 10:56:31 +1300

    chore: fix pwa settings + icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import clsx from "clsx";
 import { XIcon } from "@heroicons/react/solid";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { toast, TypeOptions } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.min.css";
@@ -30,7 +31,31 @@ function MyApp({ Component, pageProps }: AppProps) {
     });
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Wordle - a word puzzle game" />
+        <meta name="keywords" content="wordle, puzzle, game" />
+        <title>Wordle</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/icon-32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/icons/icon-192.png"></link>
+        <meta name="theme-color" content="#6aaa64" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,10 +38,6 @@ export default function Home() {
 
   return (
     <div className="flex flex-col max-w-lg w-full m-auto h-screen">
-      <Head>
-        <title>React Wordle</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Header onIconClick={actions.openModal} />
       <main className="flex-1 p-4 flex flex-col justify-between">
         {process.env.NODE_ENV === "development" && (
