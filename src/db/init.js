71cbec5f7fc6815cb41da5d08f1999d4acbff432
Alan Soares <alanrsoars@gmail.com>
2022-01-23 13:28:46 +1300

    chore: tweak db script to exclude plurals

--- src/db/init.js
+++ src/db/init.js
@@ -1,18 +1,22 @@
-import { resolve } from "path";
-import { readFile, writeFile } from "fs/promises";
-
-import { WORD_LENGTH } from "./config";
+const { resolve } = require("path");
+const { readFile, writeFile } = require("fs/promises");
 
 async function main() {
   try {
     const file = await readFile(resolve(__dirname, "wordlist.txt"), "utf-8");
 
-    const rows = file.split("\n");
+    const rows = file
+      .split("\n")
+      .map((x) => x.trim())
+      .filter((row) => /^[a-z]+$/gi.test(row));
 
-    const items = rows.filter(
-      (row) =>
-        row.trim().length === WORD_LENGTH && /^[a-z]+$/gi.test(row.trim())
-    );
+    const fourLetterWords = rows.filter((x) => x.length === 4);
+    const fiveLetterWords = rows.filter((x) => x.length === 5);
+
+    const isNotPlural = (word) =>
+      word.endsWith("s") ? !fourLetterWords.includes(word.slice(0, 4)) : true;
+
+    const items = fiveLetterWords.filter(isNotPlural);
 
     const data = { length: items.length, items };
 
